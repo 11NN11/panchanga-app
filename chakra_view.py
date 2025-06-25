4987e8c3df7ad442c1246d26ab66c0f383cc1ca8
11NN11 <152863519+11NN11@users.noreply.github.com>
2025-06-26 03:16:24 +0530

    Create chakra_view.py
    chakra_view.py (placeholder)

---
+++ chakra_view.py
@@ -0,0 +1,7 @@
+# chakra_view.py
+
+import streamlit as st
+
+def show_chakra():
+    st.subheader("🌀 Chakra View Coming Soon")
+    st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Yantra_of_Maha_Vidya_Tara.svg/512px-Yantra_of_Maha_Vidya_Tara.svg.png", caption="Placeholder for Chakra View")
